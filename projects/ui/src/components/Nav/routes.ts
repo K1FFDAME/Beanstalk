d23f850d2ae73859eb884314e09af172ea88fc10
Ермаков Сергей <71288640+K1FFDAME@users.noreply.github.com>
2024-02-10 11:55:29 +0300

    changes links a bit

--- projects/ui/src/components/Nav/routes.ts
+++ projects/ui/src/components/Nav/routes.ts
@@ -1,15 +1,7 @@
-import aboutIcon from '~/img/beanstalk/interface/nav/about.svg';
 import beanNFTIcon from '~/img/beanstalk/interface/nav/bean-nft.svg';
-import discordIcon from '~/img/beanstalk/interface/nav/discord.svg';
 import governanceIcon from '~/img/beanstalk/interface/nav/governance.svg';
 import swapIcon from '~/img/beanstalk/interface/nav/trade.svg';
-import twitterIcon from '~/img/beanstalk/interface/nav/twitter.svg';
-import immunefiIcon from '~/img/beanstalk/interface/nav/immunefi.svg';
-import docsIcon from '~/img/beanstalk/interface/nav/docs.svg';
-import disclosuresIcon from '~/img/beanstalk/interface/nav/disclosures.svg';
 import analyticsIcon from '~/img/beanstalk/interface/nav/stats.svg';
-import basinIcon from '~/img/beanstalk/interface/nav/basin.svg';
-import pipelineIcon from '~/img/beanstalk/interface/nav/pipeline.svg';
 
 export type RouteData = {
   /** Nav item title */
@@ -82,58 +74,58 @@ const ROUTES: { [key in RouteKeys]: RouteData[] } = {
       icon: governanceIcon,
       small: true,
     },
-    {
-      path: 'docs',
-      href: 'https://docs.bean.money/almanac',
-      title: 'Docs',
-      icon: docsIcon,
-      small: true,
-    },
+    // {
+    //   path: 'docs',
+    //   href: 'https://docs.bean.money/almanac',
+    //   title: 'Docs',
+    //   icon: docsIcon,
+    //   small: true,
+    // },
   ],
   // About Button
   additional: [
-    {
-      path: 'about',
-      title: 'About',
-      href: 'https://bean.money',
-      icon: aboutIcon,
-    },
-    {
-      path: 'disclosures',
-      title: 'Disclosures',
-      href: 'https://docs.bean.money/almanac/disclosures',
-      icon: disclosuresIcon,
-    },
-    {
-      path: 'bugbounty',
-      title: 'Bug Bounty',
-      href: 'https://immunefi.com/bounty/beanstalk',
-      icon: immunefiIcon,
-    },
-    {
-      path: 'discord',
-      href: 'https://discord.gg/beanstalk',
-      title: 'Discord',
-      icon: discordIcon,
-    },
-    {
-      path: 'twitter',
-      href: 'https://twitter.com/beanstalkmoney',
-      title: 'Twitter',
-      icon: twitterIcon,
-    },
-    {
-      path: 'basin',
-      href: 'https://basin.exchange',
-      title: 'Basin',
-      icon: basinIcon,
-    },
-    {
-      path: 'pipeline',
-      href: 'https://evmpipeline.org',
-      title: 'Pipeline',
-      icon: pipelineIcon,
-    },
+  //   {
+  //     path: 'about',
+  //     title: 'About',
+  //     href: 'https://bean.money',
+  //     icon: aboutIcon,
+  //   },
+  //   {
+  //     path: 'disclosures',
+  //     title: 'Disclosures',
+  //     href: 'https://docs.bean.money/almanac/disclosures',
+  //     icon: disclosuresIcon,
+  //   },
+  //   {
+  //     path: 'bugbounty',
+  //     title: 'Bug Bounty',
+  //     href: 'https://immunefi.com/bounty/beanstalk',
+  //     icon: immunefiIcon,
+  //   },
+  //   {
+  //     path: 'discord',
+  //     href: 'https://discord.gg/beanstalk',
+  //     title: 'Discord',
+  //     icon: discordIcon,
+  //   },
+  //   {
+  //     path: 'twitter',
+  //     href: 'https://twitter.com/beanstalkmoney',
+  //     title: 'Twitter',
+  //     icon: twitterIcon,
+  //   },
+  //   {
+  //     path: 'basin',
+  //     href: 'https://basin.exchange',
+  //     title: 'Basin',
+  //     icon: basinIcon,
+  //   },
+  //   {
+  //     path: 'pipeline',
+  //     href: 'https://evmpipeline.org',
+  //     title: 'Pipeline',
+  //     icon: pipelineIcon,
+  //   },
   ],
   // Market Menu
   market: [
